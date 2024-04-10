11fb4826f9ccb81860057f7842df14f85515c242
Yin <doyin@deloitte.com.cn>
2024-04-10 18:08:58 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,9 @@
+const numberArrayToString = require('./numberArrayToString')
+const capitalizeFirstLetter = require('./capitalizeFirstLetter')
+const getCurrentTimestamp = require('./getCurrentTimestamp')
+const capitalizeString = require('./capitalizeString')
+const truncateString = require('./truncateString')
+
+module.exports = (str) => {
+    return str.toLowerCase();
+  }
